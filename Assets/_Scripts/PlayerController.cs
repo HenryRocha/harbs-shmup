a9cfe1288ee7a8f3a62a9b72dff97b670c3f40ea
Henry Rocha <henryfr1@al.insper.edu.br>
2021-03-02 11:32:30 -0300

    Add basic player animations and transitions

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class PlayerController : SteerableBehaviour, IShooter, IDamageable
 {
-  
+    // Reference to the animator object.
+    private Animator animator;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    private void Start()
+    {
+        // Get the reference.
+        animator = GetComponent<Animator>();
+    }
 
     public void Shoot()
     {
@@ -21,11 +32,22 @@ public class PlayerController : SteerableBehaviour, IShooter, IDamageable
         Destroy(gameObject);
     }
 
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
     void FixedUpdate()
     {
         float yInput = Input.GetAxis("Vertical");
         float xInput = Input.GetAxis("Horizontal");
         Thrust(xInput, yInput);
+        if (yInput != 0 || xInput != 0)
+        {
+            animator.SetFloat("Velocity", 1.0f);
+        }
+        else
+        {
+            animator.SetFloat("Velocity", 0.0f);
+        }
     }    
 
     private void OnTriggerEnter2D(Collider2D collision)
