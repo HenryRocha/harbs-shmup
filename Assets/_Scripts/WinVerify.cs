cc4c878a527e66dc771e246338b07b2e8cf903a3
Henry Rocha <henryfr1@al.insper.edu.br>
2021-03-15 02:18:07 -0300

    Add you win screen

---
+++ Assets/_Scripts/WinVerify.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinVerify : MonoBehaviour
+{
+    private void Update()
+    {
+        GameObject[] enemiesAlive = GameObject.FindGameObjectsWithTag("Enemies");
+
+        if (enemiesAlive.Length == 0)
+        {
+            SceneManager.LoadScene("YouWinMenu");
+        }
+    }
+}
