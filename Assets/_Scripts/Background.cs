9800ee899d9df337e1d315e84773d45e6ea90093
Henry Rocha <henryfr1@al.insper.edu.br>
2021-03-15 19:09:59 -0300

    Add animated background

---
+++ Assets/_Scripts/Background.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    // Holds the current offset.
+    private float offset;
+
+    // Reference to this object's material.
+    private Material mat;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        // Get the reference.
+        // mat = GetComponent<MeshRenderer>().material;
+        mat = GetComponent<Renderer>().material;
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        // Offset the material based on time.
+        // Vector2 offset = mat.mainTextureOffset;
+        // offset.x += (Time.deltaTime / 20.0f);
+        // mat.mainTextureOffset = offset;
+
+        // Offset the material based on time.
+        offset += (Time.deltaTime / 20.0f);
+        mat.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
