6e6d7f143510fbba454890680b1e4fe27776e138
Henry Rocha <henryfr1@al.insper.edu.br>
2021-03-15 21:40:22 -0300

    Add time counter

---
+++ Assets/_Scripts/TimeUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeUI : MonoBehaviour
+{
+    [SerializeField] private float timePassed = 0;
+    [SerializeField] private Text timeUI;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        timePassed = 0;
+        timeUI = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        timePassed += Time.deltaTime;
+        UpdateText(timePassed);
+    }
+
+    private void UpdateText(float timePassed)
+    {
+        timeUI.text = $"{timePassed.ToString("F2")}s";
+    }
+}
--- README.md
+++ README.md
@@ -9,7 +9,7 @@
     - [x] Telas de início e fim (50)
     - [X] Placar e Vidas (50)
     - [ ] Pontos na tela do jogo (50)
-    - [ ] Tela de Pause (50)
+    - [X] Tela de Pause (50)
 - [X] Animações de Ambiente (100)
 - Time Trial
     - [ ] Contador de tempo: padrão (50)
@@ -21,6 +21,8 @@
     - [X] Música de Background (50)
     - [X] Música de Background 2 (50)
     - [X] Efeitos sonoros (100)
+
+Total de pontos: 850
 
 ## Recursos
 
